1c9929fa786cf75f109df8e4478bc3fb261a573e
Udit Nair <uditnair90@gmail.com>
2020-07-12 10:09:57 +0530

    Made Visual Changes in the XML of Palindrome

--- Factorial/app/src/main/java/com/example/factorial/Palindrome.java
+++ Factorial/app/src/main/java/com/example/factorial/Palindrome.java
@@ -2,14 +2,40 @@ package com.example.factorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Palindrome extends AppCompatActivity {
-
+    String getInput;
+    TextView resultPalindrome;
+    EditText checkPalindrome;
+    Button submitButton;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palindrome);
+        checkPalindrome=findViewById(R.id.editTextPalindrome);
+        resultPalindrome=findViewById(R.id.resultViewPalindrome);
+        submitButton=findViewById(R.id.buttonPalindrome);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getInput= "" + checkPalindrome.getText();
+                String reversed = new String();
+                for(int i=getInput.length()-1; i>=0; i--) reversed += getInput.charAt(i);
+                if(getInput==reversed){
+                    resultPalindrome.setText(new StringBuilder().append("YES").append(reversed).toString());
+                }
+                else{
+                    resultPalindrome.setText("NO"+getInput);
+                }
+            }
+        });
     }
-
 }
